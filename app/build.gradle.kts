3d2ec06b66d2acb81f5e6db834e4c12af87fa16b
matsuzaka-yuki <lyravoid.yuki@gmail.com>
2026-09-11 21:56:09 +0800

    refactor: complete miuix-only compose ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -348,8 +348,6 @@ dependencies {
     implementation(libs.androidx.webkit)
 
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.runtime.livedata)
--- app/src/main/java/me/bmax/apatch/ui/theme/Theme.kt
+++ app/src/main/java/me/bmax/apatch/ui/theme/Theme.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
@@ -109,65 +106,6 @@ fun APatchTheme(
         nightModeEnabled
     }
 
-    val colorScheme = if (!dynamicColor) {
-        if (darkTheme) {
-            when (customColorScheme) {
-                "amber" -> DarkAmberTheme
-                "blue_grey" -> DarkBlueGreyTheme
-                "blue" -> DarkBlueTheme
-                "brown" -> DarkBrownTheme
-                "cyan" -> DarkCyanTheme
-                "deep_orange" -> DarkDeepOrangeTheme
-                "deep_purple" -> DarkDeepPurpleTheme
-                "green" -> DarkGreenTheme
-                "indigo" -> DarkIndigoTheme
-                "light_blue" -> DarkLightBlueTheme
-                "light_green" -> DarkLightGreenTheme
-                "lime" -> DarkLimeTheme
-                "orange" -> DarkOrangeTheme
-                "pink" -> DarkPinkTheme
-                "purple" -> DarkPurpleTheme
-                "red" -> DarkRedTheme
-                "sakura" -> DarkSakuraTheme
-                "teal" -> DarkTealTheme
-                "yellow" -> DarkYellowTheme
-                else -> DarkBlueTheme
-            }
-        } else {
-            when (customColorScheme) {
-                "amber" -> LightAmberTheme
-                "blue_grey" -> LightBlueGreyTheme
-                "blue" -> LightBlueTheme
-                "brown" -> LightBrownTheme
-                "cyan" -> LightCyanTheme
-                "deep_orange" -> LightDeepOrangeTheme
-                "deep_purple" -> LightDeepPurpleTheme
-                "green" -> LightGreenTheme
-                "indigo" -> LightIndigoTheme
-                "light_blue" -> LightLightBlueTheme
-                "light_green" -> LightLightGreenTheme
-                "lime" -> LightLimeTheme
-                "orange" -> LightOrangeTheme
-                "pink" -> LightPinkTheme
-                "purple" -> LightPurpleTheme
-                "red" -> LightRedTheme
-                "sakura" -> LightSakuraTheme
-                "teal" -> LightTealTheme
-                "yellow" -> LightYellowTheme
-                else -> LightBlueTheme
-            }
-        }
-    } else {
-        when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkBlueTheme
-            else -> LightBlueTheme
-        }
-    }
-
     SystemBarStyle(
         darkMode = darkTheme
     )
@@ -192,14 +130,8 @@ fun APatchTheme(
     }
 
     MiuixTheme(controller = miuixThemeController) {
-        MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = {
-                MonetColorsProvider.UpdateCss()
-                content()
-            }
-        )
+        MonetColorsProvider.UpdateCss()
+        content()
     }
 }
 
